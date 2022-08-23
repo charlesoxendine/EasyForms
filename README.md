56341f692a9900d919088a7b2b65b68e0eddc0db
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-08-23 17:52:55 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EasyFormsPackage
+
+A description of this package.
---
+++ Sources/EasyFormsPackage/EasyFormsPackage.swift
@@ -0,0 +1,6 @@
+public struct EasyFormsPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
