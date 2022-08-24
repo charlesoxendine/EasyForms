6f76e867ae2165305c6e167b14d11d28599cd962
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-08-23 20:55:55 -0400

    update

--- Sources/EasyFormsPackage/Views/FormViewController.swift
+++ Sources/EasyFormsPackage/Views/FormViewController.swift
@@ -72,6 +72,8 @@ class FormViewController: UIViewController {
             self.tableView.register(UINib.init(nibName: $0, bundle: nil), forCellReuseIdentifier: $0)
         })
         
+        tableView.autoresizingMask = []
+        
         self.view.addSubview(tableView)
         let topConstraint = NSLayoutConstraint(item: self.tableView!, attribute: .top, relatedBy: .equal, toItem: self.tableView.superview, attribute: .top, multiplier: 1, constant: 0)
         topConstraint.isActive = true
